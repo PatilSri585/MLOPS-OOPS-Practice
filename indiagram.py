44955defd6ffc4500aecad107566bd220769d005
PatilSri585 <sri.patil321@gmail.com>
2026-01-12 19:49:17 +0530

    signin,signup modules completed

---
+++ indiagram.py
@@ -0,0 +1,54 @@
+class indiagram:
+    def __init__(self):
+        self.username = ''
+        self.password = ''
+        self.loggedin = False
+        self.menu()
+
+    def menu(self):
+        user_input = input("""Welcome to indiagram!! How would you like to proceed?
+                                1. Press 1 to sign-up
+                                2. Press 2 to sign-in
+                                3. Press 3 to write a post
+                                4. Press 4 to send a message to your firend
+                                5. Press any other key to exit\n""")
+
+        if user_input == '1':
+            self.signup()
+        elif user_input == '2':
+            self.signin()
+        elif user_input == '3':
+            pass
+        elif user_input == '4':
+            pass
+        else:
+            exit()
+            
+
+    def signup(self):
+        email = input("Enter your email here:  ")
+        password = input("Setup your password here:   ")
+        self.username = email
+        self.password = password
+        print('You have signed up successfully!!!')
+        print('\n')
+        self.menu()
+
+    def signin(self):
+        if self.username == '' and self.password =='':
+            print('Please sign-up by pressing 1 in the main menu')
+        else:
+            uname = input("Enter your email/User Name here:   ")
+            pwd = input('Enter your password here:   ')
+            if self.username ==uname and self.password==pwd:
+                print('You have signed in successfully!!!')
+                self.loggedin = True
+            else:
+                print("Please enter the correct credentials!!!")
+        print('\n')
+        self.menu()
+
+
+        
+
+obj = indiagram()
