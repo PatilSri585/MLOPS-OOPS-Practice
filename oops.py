870b128a8970866cf2b70d439184ade3d935a434
PatilSri585 <sri.patil321@gmail.com>
2026-01-12 17:40:48 +0530

    Baisc class created

---
+++ oops.py
@@ -0,0 +1,8 @@
+class employee:
+    def __init__(self):
+        self.id = 101
+        self.salary = 20000
+        self.designation = "Software Engineer"
+
+Harsha = employee()
+print(Harsha.salary)
