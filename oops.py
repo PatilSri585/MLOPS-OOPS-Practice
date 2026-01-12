8f9c089d90b5b3ac11ea8b3a9d9f9f8fbe658ca1
PatilSri585 <sri.patil321@gmail.com>
2026-01-12 18:11:25 +0530

    difference between class and method

--- oops.py
+++ oops.py
@@ -1,12 +1,16 @@
 class employee:
     def __init__(self):
+        print('started executing attribues/data')
         self.id = 101
         self.salary = 20000
         self.designation = "Software Engineer"
+        print('finished executing attribues/data')
 
 
     def travel(self,destination):
+        print('started executing Method')
         print(f'Employee is now travelling to {destination}')
+        print('Finished executing Method')
 
 Harsha = employee()
 Harsha.travel("Ooty")
