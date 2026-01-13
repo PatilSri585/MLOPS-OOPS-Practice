ca4caa0ef8a9b5136a4ee0f078e96b2dfb4ce286
PatilSri585 <sri.patil321@gmail.com>
2026-01-13 11:03:26 +0530

    Encapsulation example file

---
+++ encapsulation.py
@@ -0,0 +1,13 @@
+class employee:
+    def __init__(self):
+        self.name = "Employee1"     #normal
+        self.id = 123               #normal
+        self.designation = 'SDE'    #normal
+        self.__salary = 40000       #encapsulated
+
+
+harsha = employee()
+print(harsha.name)
+print(harsha.designation)
+print(harsha.id)
+print(harsha.__salary)              #cant be accessed.AttributeError: 'employee' object has no attribute '__salary'
